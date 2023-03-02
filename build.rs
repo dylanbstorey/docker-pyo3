0f50359ad94de4ab3b2b8fe092ef9a292466d5d9
Dylan Storey <dylan.storey@gmail.com>
2023-03-02 12:44:15 -0500

    rustfmt

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-/// when using this crate as a dependency in another pyo3 based package on a mac - the linker args don't propagate 
+/// when using this crate as a dependency in another pyo3 based package on a mac - the linker args don't propagate
 /// correctly - so we are explicit about this build arg for inheritence sakes.
 fn main() {
     pyo3_build_config::add_extension_module_link_args();
